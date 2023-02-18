2524c7cb3fa2b0d8e35db4ce3b0cfac44a3164c3
Josmin Rose <92521010+josmin7@users.noreply.github.com>
2023-02-18 14:20:33 +0530

    Create josmin.cpp

---
+++ josmin.cpp
@@ -0,0 +1,11 @@
+//File: josmin.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "My name is Josmin\n";
+  cout << "My SRN is PES2UG20CS147\n";
+  cout << "I have successfully built and run the tasks given\n";
+  cout << "I am doing computer science\n";
+  return 0;
+}
